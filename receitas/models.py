53ee68240857a1a1ce6e53b592bc448d8c753517
vinicius00franco <vincus.franco.silva@gmail.com>
2022-11-21 19:08:32 -0300

    cirando um relacionamento entre os bancos e migração

--- receitas/models.py
+++ receitas/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from datetime import datetime
 
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f'{self.nome}'
 
 # Create your models here.
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete= models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     tempo_preparo = models.IntegerField()
@@ -12,8 +18,9 @@ class Receita(models.Model):
     categoria = models.CharField(max_length=100)
     date_receita = models.DateTimeField(default=datetime.now,blank=True)
 
-class Pessoa(models.Model):
-    nome = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    def __str__(self):
+        return f'{self.nome_receita}'
+
+
 
 
